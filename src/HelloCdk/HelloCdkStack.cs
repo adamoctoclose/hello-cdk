bc0ddc611c92407768d4dbcdb4d71036b2cbb1c2
adamoctoclose <adam.close@octopus.com>
2022-11-28 13:40:54 +0000

    First commit

---
+++ src/HelloCdk/HelloCdkStack.cs
@@ -0,0 +1,16 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.S3;
+
+namespace HelloCdk
+{
+    public class HelloCdkStack : Stack
+    {
+        public HelloCdkStack(App scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            new Bucket(this, "MyFirstBucket", new BucketProps
+            {
+                Versioned = true
+            });
+        }
+    }
+}
